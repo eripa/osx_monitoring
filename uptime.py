84e8d886e8cd2f2b0e7d33b57c331a41c19409d7
Eric Ripa <eric@ripa.io>
2013-05-11 21:13:41 +0200

    added one additional uptime printout for unix type uptime

--- uptime.py
+++ uptime.py
@@ -5,10 +5,20 @@ import sys
 sys.path.append('/System/Library/Frameworks/Python.framework/Versions/Current/Extras/lib/python/PyObjC')
 from Foundation import *
 from datetime import datetime, timedelta
+import subprocess
 
 def main():
-    uptime_s = timedelta(seconds=int(NSProcessInfo.processInfo().systemUptime()))
-    print 'uptime_minutes:'+str(uptime_s.seconds/60)
+  # First get the OS X reported uptime (this is actual uptime as far as I understand it, excluding sleep)
+  uptime_s = timedelta(seconds=int(NSProcessInfo.processInfo().systemUptime()))
+  print 'uptime_osx_minutes:'+str(uptime_s.seconds/60),
+
+  # Then get the Unix uptime, this is the total calendar time since last boot
+  p = subprocess.Popen("uptime".split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  stdout, stderr = p.communicate()
+  uptime_days = stdout.split()[2]
+  uptime_hours, uptime_minutes = stdout.split()[4][:-1].split(":")
+  uptime = timedelta(days=int(uptime_days), hours=int(uptime_hours), minutes=int(uptime_minutes))
+  print 'uptime_unix_minutes:'+str(int(uptime.total_seconds())/60)
 
 if __name__ == '__main__':
-    main()
+  main()
