42b228b888e1b30174a114785511eb511fc3353c
Eric Ripa <eric@ripa.io>
2013-05-11 18:19:57 +0200

    tempmonitor bootstrap done

--- bootstrap.py
+++ bootstrap.py
@@ -8,6 +8,10 @@ from bs4 import BeautifulSoup
 import re
 import mechanize
 import hashlib
+import shlex
+import subprocess
+import os
+import shutil
 
 def getSHA1forTempmonitor(url):
   sha1 = None
@@ -34,8 +38,8 @@ def downloadTempMonitor(url, sha1=None):
   br.open(url)
   br.select_form(name="Download")
   response1 = br.submit()
+  print "Downloading TempMonitor..."
   with open(tempmon_archive, "w") as f:
-    print "Downloading TempMonitor..."
     f.write(response1.read())
   if sha1:
     # let's verify sha1
@@ -44,11 +48,39 @@ def downloadTempMonitor(url, sha1=None):
       print "Success!"
     else:
       print "Failed, this might cause problems later on.."
+  return tempmon_archive
+
+def extractTempMonitorArchive(archive):
+  # First convert the archive to be able to automatically mount it later
+  convert_command = "hdiutil convert -quiet %s -format UDTO -o tempmonitor.cdr" % (archive)
+  attach_command = "hdiutil attach -quiet -nobrowse -noverify -noautoopen -mountpoint tempmount tempmonitor.cdr"
+  detach_command = "hdiutil detach -force -quiet tempmount"
+  print 'Attaching image...',
+  subprocess.call(shlex.split(convert_command))
+  subprocess.call(shlex.split(attach_command))
+  print 'Done!'
+  # Copy the files that we need..
+  print 'Copying tempmonitor binary and detaching image...',
+  shutil.copy("tempmount/TemperatureMonitor.app/Contents/MacOS/tempmonitor", ".")
+  subprocess.call(shlex.split(detach_command))
+  print 'Done!'
+
+def cleanArchives(archive):
+  print "Cleaning archive...",
+  basename = archive.split('.')[0]
+  # remove both archives
+  os.remove(basename+'.cdr')
+  os.remove(basename+'.dmg')
+  print "Done!"
+
 
 def main():
   tempmonitor_url = 'http://www.bresink.de/osx/0TemperatureMonitor/download.php5'
   tempmon_sha1 = getSHA1forTempmonitor(tempmonitor_url)
-  downloadTempMonitor(tempmonitor_url, tempmon_sha1)
+  tempmon_archive = downloadTempMonitor(tempmonitor_url, tempmon_sha1)
+  extractTempMonitorArchive(tempmon_archive)
+  cleanArchives(tempmon_archive)
+
 
 if __name__ == '__main__':
   main()
