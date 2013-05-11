54463d0ae60fe2577653f381dfb7a4b7041c5954
Eric Ripa <eric@ripa.io>
2013-05-11 19:56:18 +0200

    added path to OS X python libraries

--- uptime.py
+++ uptime.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 #
 
+import sys
+sys.path.append('/System/Library/Frameworks/Python.framework/Versions/Current/Extras/lib/python/PyObjC')
 from Foundation import *
 from datetime import datetime, timedelta
 
 def main():
     uptime_s = timedelta(seconds=int(NSProcessInfo.processInfo().systemUptime()))
     print 'uptime_minutes:'+str(uptime_s.seconds/60)
-    
+
 if __name__ == '__main__':
     main()
